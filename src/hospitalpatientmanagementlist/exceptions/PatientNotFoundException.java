18efe868124d04a775fe939fba6a9fb9f5fec4b3
rbsrafa <rbsrafa@gmail.com>
2018-12-20 11:25:15 +0000

    added comments and implemented email validation

--- src/hospitalpatientmanagementlist/exceptions/PatientNotFoundException.java
+++ src/hospitalpatientmanagementlist/exceptions/PatientNotFoundException.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hospitalpatientmanagementlist.exceptions;
 
 /**
  *
- * @author rbsrafa
+ * @author 
+ * rbsrafa
+ * Lucival
  */
 public class PatientNotFoundException extends RuntimeException{
 
